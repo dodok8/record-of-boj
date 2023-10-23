6c6d0e93e73d273d9792ac77d2b7b7001c8ad593
Kim, Hyeonseo <dodok8@gmail.com>
2023-10-23 09:06:16 +0000

    Solve 1991

--- rust/1991_v1.rs
+++ rust/1991_v1.rs
@@ -1,9 +1,8 @@
-use std::collections::VecDeque;
+// 트리 순회
 use std::error::Error;
-use std::fmt::{self, Display, Formatter, Write};
+use std::fmt::Write;
 use std::io::{stdin, Read};
-
-struct Node<T: Copy> {
+struct Node<T: Clone> {
     index: usize,
     data: T,
     left: Option<usize>,
@@ -11,48 +10,133 @@ struct Node<T: Copy> {
     parent: Option<usize>,
 }
 
-impl<T> Node<T>
+struct BinaryTree<T: Clone> {
+    nodes: Vec<Node<T>>,
+    root_idx: usize,
+}
+
+impl<T> BinaryTree<T>
 where
-    T: Copy,
+    T: Clone,
 {
-    fn get_inorder(&self, nodes: &Vec<Node<T>>) -> Vec<(usize, T)> {
-        let mut travel_queue: VecDeque<usize> = VecDeque::from([self.index]);
-        let mut result: Vec<(usize, T)> = Vec::new();
-        let mut visited: Vec<bool> = vec![false; nodes.len()];
-        while travel_queue.len() != 0 {
-            let curr_idx = travel_queue.pop_front().unwrap();
-            if visited[curr_idx] {
-                break;
-            };
-            visited[curr_idx] = true;
-            let curr_node = &nodes[curr_idx];
-            result.push((curr_idx, curr_node.data));
-            match curr_node.left {
-                Some(value) => travel_queue.push_back(value),
+    fn get_preorder(&self) -> Vec<usize> {
+        let mut result: Vec<usize> = Vec::new();
+
+        fn _preorder<T: Clone>(curr_idx: usize, nodes: &Vec<Node<T>>, result: &mut Vec<usize>) {
+            result.push(curr_idx);
+            match nodes[curr_idx].left {
+                Some(value) => _preorder(value, nodes, result),
                 _ => (),
             }
-
-            match curr_node.right {
-                Some(value) => travel_queue.push_back(value),
+            match nodes[curr_idx].right {
+                Some(value) => _preorder(value, nodes, result),
                 _ => (),
             }
         }
+
+        _preorder(self.root_idx, &self.nodes, &mut result);
         result
     }
 
-    fn get_preorder(&self, nodes: &Vec<Node<T>>) -> Vec<(usize, T)> {}
+    fn get_inorder(&self) -> Vec<usize> {
+        let mut result: Vec<usize> = Vec::new();
+
+        fn _inorder<T: Clone>(curr_idx: usize, nodes: &Vec<Node<T>>, result: &mut Vec<usize>) {
+            match nodes[curr_idx].left {
+                Some(value) => _inorder(value, nodes, result),
+                _ => (),
+            }
+            result.push(curr_idx);
+            match nodes[curr_idx].right {
+                Some(value) => _inorder(value, nodes, result),
+                _ => (),
+            }
+        }
+
+        _inorder(self.root_idx, &self.nodes, &mut result);
+        result
+    }
+    fn get_postorder(&self) -> Vec<usize> {
+        let mut result: Vec<usize> = Vec::new();
+
+        fn _postorder<T: Clone>(curr_idx: usize, nodes: &Vec<Node<T>>, result: &mut Vec<usize>) {
+            match nodes[curr_idx].left {
+                Some(value) => _postorder(value, nodes, result),
+                _ => (),
+            }
+            match nodes[curr_idx].right {
+                Some(value) => _postorder(value, nodes, result),
+                _ => (),
+            }
+            result.push(curr_idx);
+        }
+
+        _postorder(self.root_idx, &self.nodes, &mut result);
+        result
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut output = String::new();
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
-    let mut input = input
-        .split_ascii_whitespace()
-        .map(str::parse::<i32>)
-        .flatten();
-    let num_nodes = input.next().unwrap() as usize;
-    writeln!(output, "Hello world").unwrap();
-    print!("{}", output);
+    let mut tokens = input.split_ascii_whitespace();
+    let num_nodes: usize = tokens.next().unwrap().parse().unwrap();
+    let mut tree: BinaryTree<usize> = BinaryTree {
+        nodes: Vec::new(),
+        root_idx: 0,
+    };
+    for idx in 0..26 {
+        tree.nodes.push(Node {
+            index: idx,
+            data: idx,
+            left: None,
+            right: None,
+            parent: None,
+        })
+    }
+    for _idx in 0..num_nodes {
+        let node_idx = get_alphabet_order(tokens.next().unwrap().chars().next().unwrap()).unwrap();
+        match get_alphabet_order(tokens.next().unwrap().chars().next().unwrap()) {
+            Some(value) => {
+                tree.nodes[node_idx].left = Some(value);
+                tree.nodes[value].parent = Some(node_idx);
+            }
+            _ => {}
+        };
+        match get_alphabet_order(tokens.next().unwrap().chars().next().unwrap()) {
+            Some(value) => {
+                tree.nodes[node_idx].right = Some(value);
+                tree.nodes[value].parent = Some(node_idx);
+            }
+            _ => {}
+        };
+    }
+    for num in tree.get_preorder() {
+        write!(output, "{}", get_alphabet_from_num(num)).unwrap();
+    }
+    writeln!(output, "").unwrap();
+    for num in tree.get_inorder() {
+        write!(output, "{}", get_alphabet_from_num(num)).unwrap();
+    }
+    writeln!(output, "").unwrap();
+    for num in tree.get_postorder() {
+        write!(output, "{}", get_alphabet_from_num(num)).unwrap();
+    }
+    writeln!(output, "").unwrap();
+
+    println!("{}", output);
     Ok(())
+}
+
+fn get_alphabet_order(alphabet: char) -> Option<usize> {
+    if alphabet == '.' {
+        None
+    } else {
+        Some(alphabet as u32 as usize - 65)
+    }
+}
+
+fn get_alphabet_from_num(num: usize) -> char {
+    (num + 65) as u8 as char
 }
