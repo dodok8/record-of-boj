b9c08fb408f27b774964611254a214e20d1d080d
Kim, Hyeonseo <dodok8@gmail.com>
2023-11-25 07:32:06 +0000

    TLE 9663

--- rust/9663_v1.rs
+++ rust/9663_v1.rs
@@ -1,69 +1,80 @@
 // N-Queen
-use std::collections::VecDeque;
 use std::error::Error;
 use std::fmt::Write;
 use std::io::{stdin, Read};
 
-fn locate_queen(queens: &mut VecDeque<(usize, usize)>, coord: (usize, usize)) {
-    queens.push_back(coord);
-}
-
-fn remove_queen(queens: &mut VecDeque<(usize, usize)>) {
-    queens.pop_back();
-}
-
-fn judge_valid_place(queens: &mut VecDeque<(usize, usize)>, coord: (usize, usize)) -> bool {
-    if queens.is_empty() {
-        return true;
-    }
-    queens
-        .iter()
-        .map(|queen| {
-            if coord.0 == queen.0 || coord.1 == queen.1 {
-                return false;
-            }
-            if coord.0.abs_diff(queen.0) == coord.1.abs_diff(queen.1) {
-                return false;
-            }
-            true
-        })
-        .reduce(|acc, curr| acc && curr)
-        .unwrap()
-}
-
-fn calc_queen(
-    queens: &mut VecDeque<(usize, usize)>,
+struct QueenStatus {
+    cols: Vec<bool>,
+    minus: Vec<bool>,
+    plus: Vec<bool>,
+    plus_d: usize,
     n: usize,
-    last_x: usize,
-    num_left_queen: usize,
-    result: &mut usize,
-) {
-    if num_left_queen == 0 {
-        *result += 1;
+}
+
+impl QueenStatus {
+    fn locate_queen(&mut self, x: usize, y: usize) {
+        self.cols[y] = false;
+        self.minus[y + x] = false;
+        self.plus[y + self.plus_d - x] = false;
+    }
+    fn remove_queen(&mut self, x: usize, y: usize) {
+        self.cols[y] = true;
+        self.minus[y + x] = true;
+        self.plus[y + self.plus_d - x] = true;
+    }
+    fn judge_valid_place(&self, x: usize, y: usize) -> bool {
+        if self.plus[y + self.plus_d - x] && self.cols[y] && self.minus[x + y] {
+            return true;
+        }
+        false
+    }
+}
+
+fn calc_queens(
+    left_over: usize,
+    last_rows: usize,
+    queen_status: &mut QueenStatus,
+) -> Result<usize, Box<dyn Error>> {
+    let n = queen_status.n;
+    if left_over == 0 {
+        Ok(1)
     } else {
-        for xdx in last_x..n {
+        let mut sum = 0;
+        for xdx in (last_rows + 1)..n {
             for ydx in 0..n {
-                if xdx == last_x && (xdx != 0) {
-                    continue;
-                }
-                if judge_valid_place(queens, (xdx, ydx)) {
-                    locate_queen(queens, (xdx, ydx));
-                    calc_queen(queens, n, xdx, num_left_queen - 1, result);
-                    remove_queen(queens);
+                if queen_status.judge_valid_place(xdx, ydx) {
+                    queen_status.locate_queen(xdx, ydx);
+                    sum += calc_queens(left_over - 1, xdx, queen_status).unwrap();
+                    queen_status.remove_queen(xdx, ydx);
                 }
             }
         }
+        Ok(sum)
     }
 }
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mut output = String::new();
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
     let mut input = input.split_ascii_whitespace().flat_map(str::parse::<usize>);
     let n = input.next().unwrap();
-    let mut queens = VecDeque::new();
+    let mut queen_status = QueenStatus {
+        cols: vec![true; n + 1],
+        minus: vec![true; 2 * n - 1],
+        plus: vec![true; 2 * n - 1],
+        plus_d: n - 1,
+        n,
+    };
     let mut result = 0;
-    calc_queen(&mut queens, n, 0, n, &mut result);
+    for x in 0..n {
+        for y in 0..n {
+            queen_status.locate_queen(x, y);
+            result += calc_queens(n - 1, x, &mut queen_status).unwrap();
+            queen_status.remove_queen(x, y);
+        }
+    }
+
     writeln!(output, "{}", result).unwrap();
     print!("{}", output);
     Ok(())
