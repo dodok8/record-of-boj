9656d837ad9f13679aee77997c52f8cb305fde5c
Kim, Hyeonseo <dodok8@gmail.com>
2023-12-05 09:26:25 +0000

    Update main.rs to Rust snippet

--- rust/main.rs
+++ rust/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut output = String::new();
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<i32>);
+    writeln!(output, "Hello world").unwrap();
+    print!("{}", output);
+    Ok(())
 }
