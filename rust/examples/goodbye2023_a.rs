97357b99d3db9c919d89198edb06622bf12cbf21
Kim, Hyeonseo <dodok8@gmail.com>
2023-12-31 11:27:35 +0000

    Solve goodbye BOJ 2023 a

---
+++ rust/examples/goodbye2023_a.rs
@@ -0,0 +1,21 @@
+use std::error::Error;
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut output = String::new();
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    for _ in 0..input.next().unwrap() {
+        let n = input.next().unwrap();
+
+        if (n + 1) % (n % 100) == 0 {
+            writeln!(output, "Good").unwrap();
+        } else {
+            writeln!(output, "Bye").unwrap();
+        }
+    }
+    print!("{}", output);
+    Ok(())
+}
