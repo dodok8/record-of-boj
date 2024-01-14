bb89e7c6fa853bdae3154c9164bdb1293edbddb3
Kim, Hyeonseo <dodok8@gmail.com>
2024-01-14 05:43:22 +0000

    Solve Hello, BOJ 2024 A

--- rust/examples/hello_2024_a_v1.rs
+++ rust/examples/hello_2024_a_v1.rs
@@ -1,5 +1,6 @@
 // 모바일 광고 입찰
 
+use std::collections::HashMap;
 use std::error::Error;
 use std::fmt::Write;
 use std::io::{stdin, Read};
@@ -8,18 +9,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut output = String::new();
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
-    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<i32>);
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<i64>);
 
     let num_ad = input.next().unwrap();
     let goal_ad = input.next().unwrap();
     let mut curr_ad = 0;
-    let mut count_deltas = vec![0; usize::pow(10, 9) + 1];
+    let mut count_deltas = HashMap::new();
     for _ in 0..num_ad {
         let a = input.next().unwrap();
         let b = input.next().unwrap();
-
-        if b - a > 0 {
-            count_deltas[(b - a) as usize] += 1;
+        let delta = b - a;
+        if delta > 0 {
+            let count = count_deltas.entry(delta).or_insert(0);
+            *count += 1;
         } else {
             curr_ad += 1;
         }
@@ -28,12 +30,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     if curr_ad >= goal_ad {
         writeln!(output, "0").unwrap();
     } else {
-        for idx in 1..(usize::pow(10, 9) + 1) {
-            count_deltas[idx] += count_deltas[idx - 1];
-            if count_deltas[idx] >= goal_ad - curr_ad {
-                writeln!(output, "{}", idx).unwrap();
+        let mut previous_key: Option<i64> = None;
+
+        let mut keys: Vec<i64> = count_deltas.keys().cloned().collect();
+        keys.sort_unstable();
+
+        for &key in keys.iter() {
+            if let Some(prev_key) = previous_key {
+                let prev_count = *count_deltas.get(&prev_key).unwrap();
+                *count_deltas.entry(key).or_insert(0) += prev_count;
+            }
+            if *count_deltas.get(&key).unwrap() >= goal_ad - curr_ad {
+                writeln!(output, "{}", key).unwrap();
                 break;
             }
+
+            previous_key = Some(key);
         }
     }
     print!("{}", output);
