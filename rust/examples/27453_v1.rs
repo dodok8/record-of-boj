2eca2b5358ba9a27d7fdff096c43fa026245d999
Kim, Hyeonseo <dodok8@gmail.com>
2024-01-18 09:47:53 +0000

    Solve 27453

--- rust/examples/27453_v1.rs
+++ rust/examples/27453_v1.rs
@@ -11,11 +11,11 @@ fn get_connected_points(
     max_h: i32,
     max_w: i32,
     map: &Vec<Vec<i32>>,
-    parents: &Vec<Vec<Vec<bool>>>,
+    parents: &mut Vec<Vec<Vec<bool>>>,
 ) -> Vec<(usize, usize)> {
     let mut connected_points: Vec<(usize, usize)> = Vec::new();
     let deltas = [(1, 0), (-1, 0), (0, 1), (0, -1)];
-    for (dh, dw) in deltas {
+    for (idx, (dh, dw)) in deltas.iter().enumerate() {
         let h = point.0 as i32 + dh;
         let w = point.1 as i32 + dw;
         if (0 <= h && h < max_h) && (0 <= w && w < max_w) {
@@ -30,9 +30,7 @@ fn get_connected_points(
                         continue;
                     }
 
-                    if parents[h][w][parent.0 * h + parent.1]
-                        && parents[h][w][point.0 * h + point.1]
-                    {
+                    if parents[idx][h][w] {
                         continue;
                     }
 
@@ -40,7 +38,8 @@ fn get_connected_points(
                     sum += map[point.0][point.1];
                     sum += map[parent.0][parent.1];
                     if hanbyeol >= sum as usize {
-                        connected_points.push((h, w))
+                        connected_points.push((h, w));
+                        parents[idx][h][w] = true;
                     }
                 }
             }
@@ -83,10 +82,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    let mut parents: Vec<Vec<Vec<bool>>> =
-        vec![vec![vec![false; height * (height - 1) + width]; width]; height];
+    let mut parents: Vec<Vec<Vec<bool>>> = vec![vec![vec![false; width]; height]; 4];
     let mut travel_queue = VecDeque::new();
-    parents[start.0][start.1][start.0 * height + start.1] = true;
     travel_queue.push_back((0, start, start));
     let mut length: i32 = -1;
     while !travel_queue.is_empty() {
@@ -103,10 +100,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             height as i32,
             width as i32,
             &map,
-            &parents,
+            &mut parents,
         ) {
-            parents[point.0][point.1][parent.0 * height + parent.1] = true;
-            parents[point.0][point.1][curr_p.0 * height + curr_p.1] = true;
             travel_queue.push_back((curr_d + 1, curr_p, point));
         }
     }
