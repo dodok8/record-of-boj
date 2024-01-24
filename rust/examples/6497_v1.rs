4f185deeae0a60db0216f6de1ff9ce84161f8a94
Kim, Hyeonseo <dodok8@gmail.com>
2024-01-24 03:06:40 +0000

    Solve 6497

--- rust/examples/6497_v1.rs
+++ rust/examples/6497_v1.rs
@@ -16,38 +16,43 @@ fn main() -> Result<(), Box<dyn Error>> {
     stdin().read_to_string(&mut input).unwrap();
     let mut input = input.split_ascii_whitespace().flat_map(str::parse::<usize>);
 
-    let num_v = input.next().unwrap();
-    let num_e = input.next().unwrap();
-    let mut edges: Vec<Vec<Edge>> = vec![vec![]; num_v + 1];
-    let mut total = 0;
-
-    for _ in 0..num_e {
-        let start = input.next().unwrap();
-        let end = input.next().unwrap();
-        let weight: Weight = input.next().unwrap();
-        edges[start].push((weight, end));
-        edges[end].push((weight, start));
-        total += weight;
-    }
-
-    let mut contained = vec![false; num_v + 1];
-    let mut edges_pq: BinaryHeap<Reverse<Edge>> = BinaryHeap::new();
-    let mut dist = 0;
-    edges_pq.push(Reverse((0, 1)));
-    while !edges_pq.is_empty() {
-        let Reverse((curr_w, curr_v)) = edges_pq.pop().unwrap();
-        if contained[curr_v] {
-            continue;
+    loop {
+        let num_v = input.next().unwrap();
+        let num_e = input.next().unwrap();
+        if num_v == 0 && num_e == 0 {
+            break;
         }
-        contained[curr_v] = true;
-        dist += curr_w;
-        for &(next_w, next_v) in &edges[curr_v] {
-            if !contained[next_v] {
-                edges_pq.push(Reverse((next_w, next_v)));
+        let mut edges: Vec<Vec<Edge>> = vec![vec![]; num_v + 1];
+        let mut total = 0;
+
+        for _ in 0..num_e {
+            let start = input.next().unwrap();
+            let end = input.next().unwrap();
+            let weight: Weight = input.next().unwrap();
+            edges[start].push((weight, end));
+            edges[end].push((weight, start));
+            total += weight;
+        }
+
+        let mut contained = vec![false; num_v + 1];
+        let mut edges_pq: BinaryHeap<Reverse<Edge>> = BinaryHeap::new();
+        let mut dist = 0;
+        edges_pq.push(Reverse((0, 1)));
+        while !edges_pq.is_empty() {
+            let Reverse((curr_w, curr_v)) = edges_pq.pop().unwrap();
+            if contained[curr_v] {
+                continue;
+            }
+            contained[curr_v] = true;
+            dist += curr_w;
+            for &(next_w, next_v) in &edges[curr_v] {
+                if !contained[next_v] {
+                    edges_pq.push(Reverse((next_w, next_v)));
+                }
             }
         }
+        writeln!(output, "{}", total - dist).unwrap();
     }
-    writeln!(output, "{}", total - dist).unwrap();
     print!("{}", output);
     Ok(())
 }
