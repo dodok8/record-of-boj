74d67faa529084dd3c710139dcff6ac8548ab52b
Kim, Hyeonseo <dodok8@gmail.com>
2024-01-31 08:20:01 +0000

    WA 9252

--- rust/examples/9252_v1.rs
+++ rust/examples/9252_v1.rs
@@ -4,65 +4,55 @@ use std::fmt::Write;
 use std::io::{stdin, Read};
 
 trait LongestCommonSubsequence {
-    fn get_len_lcs(&self, other: &str) -> u64;
     fn get_lcs(&self, other: &str) -> (usize, String);
 }
 
 impl LongestCommonSubsequence for String {
-    fn get_len_lcs(&self, other: &str) -> u64 {
-        let mut cache = vec![vec![0_u64; other.len() + 1]; self.len() + 1];
-        let mut longest_idx: (usize, usize) = (0, 0);
-        for (self_idx, self_char) in self.chars().enumerate() {
-            for (other_idx, other_char) in other.chars().enumerate() {
-                if self_char == other_char {
-                    cache[self_idx + 1][other_idx + 1] = cache[self_idx][other_idx] + 1;
-                    if cache[self_idx + 1][other_idx + 1] > cache[longest_idx.0][longest_idx.1] {
-                        longest_idx = (self_idx + 1, other_idx + 1);
-                    }
-                } else {
-                    cache[self_idx + 1][other_idx + 1] = *[
-                        cache[self_idx][other_idx + 1],
-                        cache[self_idx + 1][other_idx],
-                    ]
-                    .iter()
-                    .max()
-                    .unwrap();
-                }
-            }
-        }
-        cache[longest_idx.0][longest_idx.1]
-    }
-
     fn get_lcs(&self, other: &str) -> (usize, String) {
-        let mut cache: Vec<Vec<(usize, String)>> =
-            vec![vec![(0, "".to_string()); other.len() + 1]; self.len() + 1];
+        let mut cache = vec![vec![(0, (0, 0), false); other.len() + 1]; self.len() + 1];
         let mut longest_idx: (usize, usize) = (0, 0);
-        for (self_idx, self_char) in (1..).zip(self.chars()) {
-            for (other_idx, other_char) in (1..).zip(other.chars()) {
+        let mut curr_len = 0;
+        for (self_idx, self_char) in (1..).zip(self.bytes()) {
+            for (other_idx, other_char) in (1..).zip(other.bytes()) {
                 if self_char == other_char {
                     cache[self_idx][other_idx].0 = cache[self_idx - 1][other_idx - 1].0 + 1;
-                    cache[self_idx][other_idx].1 =
-                        format!("{}{}", cache[self_idx - 1][other_idx - 1].1, self_char);
-
-                    if cache[self_idx][other_idx].0 > cache[longest_idx.0][longest_idx.1].0 {
-                        longest_idx = (self_idx, other_idx);
+                    if cache[self_idx][other_idx].0 == 1 {
+                        cache[self_idx][other_idx].1 = (self_idx, other_idx);
+                    } else {
+                        cache[self_idx][other_idx].1 = (self_idx - 1, other_idx - 1);
                     }
-                } else if cache[self_idx][other_idx - 1].0 > cache[self_idx - 1][other_idx].0 {
-                    cache[self_idx][other_idx].0 = cache[self_idx][other_idx - 1].0;
-                    cache[self_idx][other_idx].1 = cache[self_idx][other_idx - 1].1.clone();
-                } else {
+                    cache[self_idx][other_idx].2 = true;
+
+                    if cache[self_idx][other_idx].0 > curr_len {
+                        longest_idx = (self_idx, other_idx);
+                        curr_len = cache[self_idx][other_idx].0;
+                    }
+                } else if cache[self_idx - 1][other_idx].0 > cache[self_idx][other_idx - 1].0 {
                     cache[self_idx][other_idx].0 = cache[self_idx - 1][other_idx].0;
-                    cache[self_idx][other_idx].1 = cache[self_idx - 1][other_idx].1.clone();
+                    cache[self_idx][other_idx].1 = cache[self_idx - 1][other_idx].1;
+                } else {
+                    cache[self_idx][other_idx].0 = cache[self_idx][other_idx - 1].0;
+                    cache[self_idx][other_idx].1 = cache[self_idx][other_idx - 1].1;
                 }
             }
         }
 
-        (
-            cache[longest_idx.0][longest_idx.1].0,
-            cache[longest_idx.0][longest_idx.1].1.clone(),
-        )
+        let mut result = Vec::new();
+
+        loop {
+            let next = cache[longest_idx.0][longest_idx.1].1;
+            if cache[longest_idx.0][longest_idx.1].2 {
+                result.push(self.chars().nth(longest_idx.0 - 1).unwrap())
+            }
+            if longest_idx == next {
+                break (curr_len, result.iter().rev().collect());
+            } else {
+                longest_idx = next;
+            }
+        }
     }
 }
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mut output = String::new();
     let mut input = String::new();
@@ -70,9 +60,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut input = input.split_ascii_whitespace();
     let first_str = input.next().unwrap().to_string();
     let second_str = input.next().unwrap().to_string();
-    let (length, lcs) = first_str.get_lcs(&second_str);
-    writeln!(output, "{}", length).unwrap();
-    writeln!(output, "{}", lcs).unwrap();
+    let (len, str) = first_str.get_lcs(&second_str);
+    writeln!(output, "{}", len).unwrap();
+    writeln!(output, "{}", str).unwrap();
     print!("{}", output);
     Ok(())
 }
