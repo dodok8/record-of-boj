5eedac27ccd4f6a0d720197ea54b28affee6dd98
Kim, Hyeonseo <dodok8@gmail.com>
2024-02-03 06:15:25 +0000

    WA grand arena #4 A

--- rust/examples/grand_arena_4_a_v1.rs
+++ rust/examples/grand_arena_4_a_v1.rs
@@ -12,34 +12,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     let n = input.next().unwrap() as usize;
 
     let mut origin = vec![0];
-    let mut sorted = vec![0];
     for idx in 0..n {
         origin.push(input.next().unwrap());
-        sorted.push((idx + 1) as u128);
     }
 
-    let mut cache = vec![vec![0_usize; n + 1]; n + 1];
+    let mut cache = vec![0; n + 1];
     let mut final_contained = vec![];
-    for (idx, o_num) in (0..).zip(origin.clone()).skip(1) {
-        for (jdx, s_num) in (0..).zip(sorted.clone()).skip(1) {
-            if origin[idx - 1] > o_num {
-                if o_num == s_num {
-                    cache[jdx][idx] = 1;
-                } else {
-                    cache[jdx][idx] = 0;
-                }
-            } else if o_num == s_num {
-                cache[jdx][idx] = cache[jdx - 1][idx - 1] + 1;
-            } else {
-                cache[jdx][idx] = *[cache[jdx - 1][idx], cache[jdx][idx - 1]]
-                    .iter()
-                    .max()
-                    .unwrap();
-            }
+    for (idx, &o_num) in origin.iter().enumerate().skip(1) {
+        if o_num > origin[idx - 1] {
+            cache[idx] = cache[idx - 1] + 1;
+        } else {
+            cache[idx] = 1;
+        }
 
-            if o_num == s_num && (idx == n || o_num > origin[idx + 1]) {
-                final_contained.push(cache[jdx][idx]);
-            }
+        if idx == n || origin[idx + 1] < o_num {
+            final_contained.push(cache[idx]);
         }
     }
     writeln!(
