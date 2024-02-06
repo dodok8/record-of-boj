37bcffeffb93d7d76f89b518399bb1aed53c9963
Kim, Hyeonseo <dodok8@gmail.com>
2024-02-06 08:09:30 +0000

    RTE 27923

---
+++ rust/examples/27923_v1.rs
@@ -0,0 +1,36 @@
+// 햄버거최대 몇개드실수있나요?
+
+use std::error::Error;
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut output = String::new();
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let num_h = input.next().unwrap();
+    let num_c = input.next().unwrap();
+    let l = input.next().unwrap();
+    let mut cokes = vec![0_usize; num_h];
+    let mut hamburgers = vec![];
+
+    for _ in 0..num_h {
+        hamburgers.push(input.next().unwrap());
+    }
+    for _ in 0..num_c {
+        let t = input.next().unwrap() - 1;
+        for idx in t..(t + l) {
+            cokes[idx] += 1;
+        }
+    }
+    cokes.sort_unstable();
+    hamburgers.sort_unstable();
+    let mut sum = 0;
+    for (coke, hamburger) in cokes.iter().zip(hamburgers) {
+        sum += hamburger >> if *coke > 32 { 32 } else { *coke };
+    }
+    writeln!(output, "{}", sum).unwrap();
+    print!("{}", output);
+    Ok(())
+}
