b105fe78735bbbd9f72c8b8f17302913ba6e5e21
Kim, Hyeonseo <dodok8@gmail.com>
2024-02-15 20:00:55 +0900

    Solve 17352

--- rust/examples/17352_v1.rs
+++ rust/examples/17352_v1.rs
@@ -4,38 +4,43 @@ use std::error::Error;
 use std::fmt::Write;
 use std::io::{stdin, Read};
 
+fn find_parent(parents: &mut Vec<usize>, idx: usize) -> usize {
+    if idx != parents[idx] {
+        parents[idx] = find_parent(parents, parents[idx]);
+    }
+    parents[idx]
+}
+
+fn union(x: usize, y: usize, parents: &mut Vec<usize>) {
+    let x = find_parent(parents, x);
+    let y = find_parent(parents, y);
+    if x < y {
+        parents[y] = x;
+    } else {
+        parents[x] = y;
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mut output = String::new();
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
     let mut input = input.split_ascii_whitespace().flat_map(str::parse::<usize>);
     let n = input.next().unwrap();
-    let mut parents: Vec<Option<usize>> = vec![None; n + 1];
+    let mut parents: Vec<usize> = (0..(n + 1)).collect();
+
     for _ in 1..=(n - 2) {
         let a = input.next().unwrap();
         let b = input.next().unwrap();
-        // println!("{} {}", a, b);
-        if a < b {
-            if let Some(val) = parents[a] {
-                parents[b] = Some(val);
-            } else {
-                parents[b] = Some(a);
-            }
-        } else if let Some(val) = parents[b] {
-            parents[a] = Some(val);
-        } else {
-            parents[a] = Some(b);
+        union(a, b, &mut parents);
+    }
+
+    for (i, parent) in parents.iter().enumerate().skip(1) {
+        if i == *parent {
+            write!(output, "{} ", i).unwrap();
         }
     }
 
-    for (idx, num) in parents.iter().enumerate().skip(1) {
-        if let Some(_val) = num {
-            continue;
-        } else {
-            write!(output, "{} ", idx).unwrap();
-        }
-    }
-    
     println!("{}", output);
     Ok(())
 }
