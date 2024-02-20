68d034a01c8efba29e559ab2e600974d25e17242
Kim, Hyeonseo <dodok8@gmail.com>
2024-02-20 03:13:49 +0000

    WA 4% 2133

---
+++ rust/examples/2133_v1.rs
@@ -0,0 +1,35 @@
+// 타일 채우기
+
+use std::error::Error;
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn get_count(x: usize, cache: &mut Vec<Option<usize>>) -> usize {
+    if let Some(val) = cache[x] {
+        val
+    } else {
+        let mut curr_x = get_count(x - 1, cache) * get_count(1, cache);
+        let mut sum = 0;
+        for val in 0..(x - 1) {
+            sum += get_count(val, cache) * 2;
+        }
+        curr_x += sum;
+
+        cache[x] = Some(curr_x);
+        curr_x
+    }
+}
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut output = String::new();
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let n = input.next().unwrap() / 2;
+    let mut cache = vec![None; n + 1];
+    cache[0] = Some(1_usize);
+    cache[1] = Some(3);
+
+    writeln!(output, "{}", get_count(n, &mut cache)).unwrap();
+    print!("{}", output);
+    Ok(())
+}
