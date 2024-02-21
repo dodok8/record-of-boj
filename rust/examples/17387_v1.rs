e2527ba54644bebac0ce44f05537f4ed7c58a23d
Kim, Hyeonseo <dodok8@gmail.com>
2024-02-21 05:33:44 +0000

    WA 1% 17387

--- rust/examples/17387_v1.rs
+++ rust/examples/17387_v1.rs
@@ -30,6 +30,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         let ccw_34 = get_ccw(&p3, &p4, &p1) * get_ccw(&p3, &p4, &p2);
 
         if ccw_12 == 0 && ccw_34 == 0 {
+            if (p1.0 - p3.0) * (p1.0 - p4.0) < 0 || (p2.0 - p3.0) * (p2.0 - p4.0) < 0 {
+                crossed = true;
+            }
+            if (p1.1 - p3.1) * (p1.1 - p4.1) < 0 || (p2.1 - p3.1) * (p2.1 - p4.1) < 0 {
+                crossed = true;
+            }
         } else {
             crossed = ccw_12 <= 0 && ccw_34 <= 0;
         }
