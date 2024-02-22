03e37956a5110f8feb10e84172f55fa7585227e9
Kim, Hyeonseo <dodok8@gmail.com>
2024-02-22 03:41:40 +0000

    Solve 12781

--- rust/examples/12781_v1.rs
+++ rust/examples/12781_v1.rs
@@ -1,6 +1,5 @@
 // PIZZA ALVOLOC
 
-use std::cmp;
 use std::error::Error;
 use std::io::{stdin, Read};
 
@@ -23,27 +22,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let p3 = (input.next().unwrap(), input.next().unwrap());
     let p4 = (input.next().unwrap(), input.next().unwrap());
 
-    let mut crossed = false;
-    if p1 == p3 || p1 == p4 || p2 == p3 || p2 == p4 {
-        crossed = true;
-    } else {
-        let ccw_12 = get_ccw(&p1, &p2, &p3) * get_ccw(&p1, &p2, &p4);
-        let ccw_34 = get_ccw(&p3, &p4, &p1) * get_ccw(&p3, &p4, &p2);
+    let ccw_12 = get_ccw(&p1, &p2, &p3) * get_ccw(&p1, &p2, &p4);
+    let ccw_34 = get_ccw(&p3, &p4, &p1) * get_ccw(&p3, &p4, &p2);
 
-        if ccw_12 == 0 && ccw_34 == 0 {
-            if cmp::min(p1.0, p2.0) <= cmp::max(p3.0, p4.0)
-                && cmp::min(p3.0, p4.0) <= cmp::max(p1.0, p2.0)
-                && cmp::min(p1.1, p2.1) <= cmp::max(p3.1, p4.1)
-                && cmp::min(p3.1, p4.1) <= cmp::max(p1.1, p2.1)
-            {
-                crossed = true;
-            }
-        } else {
-            crossed = ccw_12 <= 0 && ccw_34 <= 0;
-        }
-    }
-
-    if crossed {
+    if ccw_12 < 0 && ccw_34 < 0 {
         println!("1");
     } else {
         println!("0");
