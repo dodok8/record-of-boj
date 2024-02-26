0bff232f25cc8a5c20e2692cc4fa024728c8cf76
Kim, Hyeonseo <dodok8@gmail.com>
2024-02-26 10:17:54 +0000

    WA 3% 2230

---
+++ rust/examples/2230_v1.rs
@@ -0,0 +1,45 @@
+// 수 고르기
+use std::error::Error;
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut output = String::new();
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<i64>);
+    let n = input.next().unwrap();
+    let m = input.next().unwrap();
+    let mut nums = Vec::new();
+
+    for _in in 0..n {
+        nums.push(input.next().unwrap());
+    }
+    nums.sort_unstable();
+
+    let mut start = 0_usize;
+    let mut end = (n - 1) as usize;
+    let mut closest_diff = i64::MAX;
+    loop {
+        let diff = nums[end] - nums[start];
+        match diff.cmp(&m) {
+            std::cmp::Ordering::Greater => {
+                closest_diff = diff;
+                start += 1;
+            }
+            std::cmp::Ordering::Equal => {
+                closest_diff = diff;
+                break;
+            }
+            std::cmp::Ordering::Less => {
+                end -= 1;
+            }
+        }
+        if start >= end {
+            break;
+        }
+    }
+    writeln!(output, "{}", closest_diff).unwrap();
+    print!("{}", output);
+    Ok(())
+}
