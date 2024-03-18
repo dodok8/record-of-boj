5ad0862b27aba1712e39b2cb7bca60b0c79ae0d8
Kim, Hyeonseo <dodok8@gmail.com>
2024-03-18 05:23:08 +0000

    WA 13% 2107

--- rust/examples/2107_v1.rs
+++ rust/examples/2107_v1.rs
@@ -23,6 +23,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         points.push(second);
     }
 
+    // 정렬 후, 정렬 순서에 따라 좌표 압축 진행.
     points.sort_unstable();
 
     let mut compressed_points = HashMap::new();
@@ -31,21 +32,22 @@ fn main() -> Result<(), Box<dyn Error>> {
         compressed_points.insert(point, idx);
     }
 
-    let mut compressed_ranges = vec![0; 2 * n + 1];
+    // 압축된 좌표를 바탕으로
+    let mut imos = vec![0; 2 * n + 1];
     for (first, second) in ranges {
         let &start = compressed_points.get(&first).unwrap();
         let &end = compressed_points.get(&second).unwrap();
 
-        compressed_ranges[start] += 1;
-        compressed_ranges[end + 1] -= 1;
+        imos[start] += 1;
+        imos[end] -= 1;
     }
 
     for idx in 1..=2 * n {
-        compressed_ranges[idx] += compressed_ranges[idx - 1];
+        imos[idx] += imos[idx - 1];
     }
 
     // 자기 자신은 포함에서 제외해야 하므로 1을 뺀다.
-    writeln!(output, "{}", compressed_ranges.iter().max().unwrap() - 1).unwrap();
+    writeln!(output, "{}", imos.iter().max().unwrap() - 1).unwrap();
     print!("{}", output);
     Ok(())
 }
