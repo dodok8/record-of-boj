0c4fcfa695daf223588727790b4eca33a2c0b241
Kim, Hyeonseo <dodok8@gmail.com>
2024-03-21 20:40:28 +0900

    RTE 10807

---
+++ rust/examples/10807_v1.rs
@@ -0,0 +1,27 @@
+//개수 세기
+
+use std::collections::HashMap;
+use std::error::Error;
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut output = String::new();
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<i32>);
+
+    let n = input.next().unwrap() as usize;
+    let mut counts: HashMap<i32, i32> = HashMap::new();
+    for _ in 0..n {
+        let k = input.next().unwrap();
+        if let Some(val) = counts.get_mut(&k) {
+            *val += 1;
+        } else {
+            counts.insert(k, 1);
+        }
+    }
+    writeln!(output, "{}", counts[&input.next().unwrap()]).unwrap();
+    print!("{}", output);
+    Ok(())
+}
