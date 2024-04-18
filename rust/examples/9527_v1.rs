42c65ba7d467c7d9a5913dca85d8567016710877
Kim, Hyeonseo <dodok8@gmail.com>
2024-04-18 11:19:00 +0000

    WA 25% 9527

--- rust/examples/9527_v1.rs
+++ rust/examples/9527_v1.rs
@@ -4,6 +4,18 @@ use std::error::Error;
 use std::fmt::Write;
 use std::io::{stdin, Read};
 
+fn count_middle(x: usize, prefix_sum_one: &Vec<usize>) -> usize {
+    if (x as f32 + 1.).log2().ceil() < 1_f32 {
+        return 0;
+    }
+    let log2 = (x as f32 + 1.).log2().ceil() as usize - 1;
+    let pow = 2_usize.pow(log2 as u32);
+    let mut result = prefix_sum_one[log2];
+    result += x - pow + 1;
+    result += count_middle(x - pow, prefix_sum_one);
+    result
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mut output = String::new();
     let mut input = String::new();
@@ -12,22 +24,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     let start = input.next().unwrap();
     let end = input.next().unwrap();
 
-    let mut count_one = vec![0; end + 1];
-    count_one[1] = 1;
-    count_one[2] = 1;
-    let mut result = 0;
-    for idx in 0..=end {
-        if idx >= 3 {
-            let log2 = (idx as f32 + 1.).log2().ceil() as usize - 1;
-            let pow = 2_usize.pow(log2 as u32);
+    let mut prefix_sum_one = vec![0_usize; 56];
+    prefix_sum_one[1] = 1;
+    prefix_sum_one[2] = 4;
+    // 중간값이 구해질 때 구하는 식으로
+    let start_log2 = (start as f32 + 1.).log2().ceil() as usize - 1;
+    let end_log2 = (end as f32 + 1.).log2().ceil() as usize - 1;
+    writeln!(output, "{} {}", start_log2, end_log2).unwrap();
 
-            count_one[idx] = count_one[idx - pow] + 1;
-        }
-        if idx >= start {
-            result += count_one[idx];
-        }
+    for idx in 3..56 {
+        prefix_sum_one[idx] = 2 * prefix_sum_one[idx - 1] + 2_usize.pow((idx - 1) as u32);
     }
-    writeln!(output, "{}", result).unwrap();
-    print!("{}", output);
+
+    println!(
+        "{}",
+        count_middle(end, &prefix_sum_one) - count_middle(start - 1, &prefix_sum_one)
+    );
     Ok(())
 }
