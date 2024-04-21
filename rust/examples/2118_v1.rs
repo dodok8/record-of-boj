c21678775de3b9c5f7eaab60b49459c5cf2cfb21
Kim, Hyeonseo <dodok8@gmail.com>
2024-04-21 05:31:03 +0000

    TLE 2% 2118

---
+++ rust/examples/2118_v1.rs
@@ -0,0 +1,40 @@
+// 두 개의 탑
+
+use std::cmp::{max, min};
+use std::error::Error;
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut output = String::new();
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let n = input.next().unwrap();
+    let mut distances = vec![0_usize];
+    for idx in 1..=n {
+        let curr = input.next().unwrap();
+        distances.push(distances[idx - 1] + curr);
+    }
+
+    let mut ans = usize::MIN;
+    for idx in 1..(n / 2 + 1) {
+        for jdx in idx + 1..n {
+            if jdx == n {
+                break;
+            }
+
+            ans = max(
+                ans,
+                min(
+                    distances[jdx] - distances[idx],
+                    distances[n] - distances[jdx] + distances[idx],
+                ),
+            );
+        }
+    }
+
+    writeln!(output, "{}", ans).unwrap();
+    print!("{}", output);
+    Ok(())
+}
