e2feaec838f200dff79f3da7350078b64003d5c9
Kim, Hyeonseo <dodok8@gmail.com>
2024-09-15 11:49:38 +0000

    AC 10999

--- rust/examples/10999_v1.rs
+++ rust/examples/10999_v1.rs
@@ -1,33 +1,59 @@
-// Incorrect code
+// 구간 합 구하기 2
+trait ArqSpec {
+    type S: Clone + Display + Debug + Default + PartialEq;
 
-type T = i64;
-struct Arq {
-    val: Vec<i64>,
-    lazy: Vec<i64>,
+    fn op(a: &Self::S, b: &Self::S) -> Self::S;
+    fn identity() -> Self::S;
+    fn combine(a: &Self::S, b: &Self::S) -> Self::S;
+    fn default() -> Self::S;
+}
+
+enum AssignSum {}
+
+impl ArqSpec for AssignSum {
+    type S = i64;
+    fn op(&a: &Self::S, &b: &Self::S) -> Self::S {
+        a + b
+    }
+
+    fn identity() -> Self::S {
+        0
+    }
+
+    fn default() -> Self::S {
+        0
+    }
+
+    fn combine(&a: &Self::S, &b: &Self::S) -> Self::S {
+        a + b
+    }
+}
+
+struct StaticArq<T: ArqSpec> {
+    val: Vec<T::S>,
+    lazy: Vec<T::S>,
     n: usize,
     h: u32,
 }
 
-impl Arq {
-    fn combine(l: T, r: T) -> T {
-        l + r
-    }
-
-    fn new(n: usize, data: impl IntoIterator<Item = i64>) -> Self {
+impl<T: ArqSpec> StaticArq<T> {
+    fn new(n: usize, data: impl IntoIterator<Item = T::S>) -> Self {
         let mut val = vec![T::default(); n];
         val.extend(data);
         let lazy = vec![T::default(); n];
         let h = 64 - n.leading_zeros();
-        for idx in (1..n).rev() {
-            val[idx] = Self::combine(val[idx << 1], val[idx << 1 | 1]);
+        for i in (1..n).rev() {
+            val[i] = T::combine(&val[i << 1], &val[i << 1 | 1]);
         }
         Self { val, lazy, n, h }
     }
 
-    fn apply(&mut self, idx: usize, len: usize, amount: T) {
-        self.val[idx] += len as T * amount;
+    fn apply(&mut self, idx: usize, len: usize, diff: &T::S) {
+        for _ in 0..len {
+            self.val[idx] = T::op(&self.val[idx], diff);
+        }
         if idx < self.n {
-            self.lazy[idx] += amount;
+            self.lazy[idx] = T::op(&self.lazy[idx], diff);
         }
     }
 
@@ -35,9 +61,12 @@ impl Arq {
         let mut len = 2;
         while idx > 1 {
             idx >>= 1;
-            self.val[idx] = Self::combine(self.val[idx << 1], self.val[idx << 1 | 1]);
+            self.val[idx] = T::combine(&self.val[idx << 1], &self.val[idx << 1 | 1]);
             if self.lazy[idx] != T::default() {
-                self.val[idx] += len as T * self.lazy[idx];
+                for _ in 0..len {
+                    self.val[idx] = T::op(&self.val[idx], &self.lazy[idx]);
+                }
+
             }
             len <<= 1;
         }
@@ -48,15 +77,16 @@ impl Arq {
         for shift in (1..=self.h).rev() {
             let ptr = idx >> shift;
             if self.lazy[ptr] != T::default() {
-                self.apply(ptr << 1, len, self.lazy[ptr]);
-                self.apply(ptr << 1 | 1, len, self.lazy[ptr]);
-                self.lazy[ptr] = 0;
+                let lazy_val = self.lazy[ptr].clone();
+                self.apply(ptr << 1, len, &lazy_val);
+                self.apply(ptr << 1 | 1, len, &lazy_val);
+                self.lazy[ptr] = T::default();
             }
             len >>= 1;
         }
     }
 
-    fn modify(&mut self, mut start: usize, mut end: usize, diff: T) {
+    fn modify(&mut self, mut start: usize, mut end: usize, diff: T::S) {
         start += self.n;
         end += self.n;
 
@@ -70,12 +100,12 @@ impl Arq {
 
         while p_start < p_end {
             if p_start & 1 == 1 {
-                self.apply(p_start, len, diff);
+                self.apply(p_start, len, &diff);
                 p_start += 1;
             }
             if p_end & 1 == 1 {
                 p_end -= 1;
-                self.apply(p_end, len, diff);
+                self.apply(p_end, len, &diff);
             }
             p_end >>= 1;
             p_start >>= 1;
@@ -87,22 +117,22 @@ impl Arq {
         }
     }
 
-    fn query(&mut self, start: usize, end: usize) -> T {
+    fn query(&mut self, start: usize, end: usize) -> T::S {
         let mut p_start = start + self.n;
         let mut p_end = end + self.n;
         self.propagate(p_start);
         if start < end {
             self.propagate(p_end - 1);
         }
-        let mut result = T::default();
+        let mut result = T::identity();
         while p_start < p_end {
             if p_start & 1 == 1 {
-                result = Self::combine(result, self.val[p_start]);
+                result = T::combine(&result, &self.val[p_start]);
                 p_start += 1;
             }
             if p_end & 1 == 1 {
                 p_end -= 1;
-                result = Self::combine(self.val[p_end], result);
+                result = T::combine(&result, &self.val[p_end]);
             }
             p_start >>= 1;
             p_end >>= 1;
@@ -113,7 +143,7 @@ impl Arq {
 }
 
 use std::error::Error;
-use std::fmt::Write;
+use std::fmt::{Debug, Display, Write};
 use std::io::{stdin, Read};
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -127,14 +157,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let k = input.next().unwrap() as usize;
 
     let data = input.by_ref().take(n);
-    let mut tree = Arq::new(n, data);
+    let mut tree: StaticArq<AssignSum> = StaticArq::new(n, data);
 
     for _ in 0..(m + k) {
-        if input.by_ref().next().unwrap() == 1 {
+        if input.next().unwrap() == 1 {
             tree.modify(
-                input.by_ref().next().unwrap() as usize - 1,
-                input.by_ref().next().unwrap() as usize,
-                input.by_ref().next().unwrap(),
+                input.next().unwrap() as usize - 1,
+                input.next().unwrap() as usize,
+                input.next().unwrap(),
             );
         } else {
             writeln!(
