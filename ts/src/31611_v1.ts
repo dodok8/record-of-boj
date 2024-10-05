ad093ad0eebd6443b7bb3f35bdca3912c246a5fe
Kim, Hyeonseo <dodok8@gmail.com>
2024-10-05 09:17:55 +0000

    AC 31611

---
+++ ts/src/31611_v1.ts
@@ -0,0 +1,28 @@
+"const fs = require('fs')",
+
+"function* inputIterator<T>(arr: Array<T>) {",
+"  for (const num of arr) {",
+"    yield num",
+"  }",
+"}",
+
+"function main() {",
+"  let input_path = '/dev/stdin'",
+"  try {",
+"    if (process.env.LOCAL) {",
+"      input_path = '/workspaces/record-of-boj/input/31611_v1'",
+"    }",
+"  } catch {}",
+"  let input = inputIterator(",
+"    fs",
+"      .readFileSync(input_path)",
+"      .toString()",
+"      .split(/\\s/)",
+"      .map((x: any) => Number(x))",
+"  )",
+"  let x = input.next().value as number",
+
+"  console.log(x % 7 == 2 ? '1' : '0')",
+"}",
+
+"main()",
