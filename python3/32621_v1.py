958df5463acdabfd5c01e7af34843122c40ac88c
Kim, Hyeonseo <dodok8@gmail.com>
2024-11-16 10:38:48 +0000

    AC 32621

--- python3/32621_v1.py
+++ python3/32621_v1.py
@@ -5,10 +5,10 @@ from sys import stdin
 
 read = lambda: stdin.readline().rstrip()
 
-exp = re.compile(r"([1-9])([0-9])*\+([1-9])([0-9])*")
-if (
-    exp.fullmatch(read()) is not None
-):
+exp = re.compile(r"^([1-9][0-9]*)\+([1-9][0-9]*)$")
+match = exp.match(read())
+
+if match and match.group(1) == match.group(2):
     print("CUTE")
 else:
     print("No Money")
