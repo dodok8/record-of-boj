c9444b409258030eaf6ce5f06756572374304070
Kim, Hyeonseo <dodok8@gmail.com>
2025-01-04 06:12:38 +0000

    WA 3943

---
+++ rust/examples/3943_v1.rs
@@ -0,0 +1,33 @@
+// 헤일스톤 수열
+
+use std::error::Error;
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut output = String::new();
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<usize>);
+
+    let t = input.next().unwrap();
+
+    for _ in 0..t {
+        let mut n = input.next().unwrap();
+        let mut max_n = n;
+        while n % 10 != 1 {
+            if n % 2 == 0 {
+                n /= 2;
+            } else {
+                n = n * 3 + 1
+            }
+
+            if max_n < n {
+                max_n = n;
+            }
+        }
+        writeln!(output, "{}", max_n)?;
+    }
+    print!("{}", output);
+    Ok(())
+}
