7d7d4dc5e1a5ab0a0e738d0eef42d12c297a03ed
Kim, Hyeonseo <dodok8@gmail.com>
2025-02-14 11:21:38 +0900

    WA 17% 31738

---
+++ rust/examples/31738_v1.rs
@@ -0,0 +1,37 @@
+// 매우 어려운 문제
+
+use std::error::Error;
+use std::fmt::Write;
+use std::io::{stdin, Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut output = String::new();
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<usize>);
+
+    let n = input.next().unwrap();
+    let m = input.next().unwrap();
+
+    if n >= m {
+        writeln!(output, "0")?;
+        print!("{}", output);
+        return Ok(());
+    }
+
+    let ans = {
+        let mut facto = 1_usize;
+        let mut idx = 1_usize;
+
+        loop {
+            facto *= idx % m;
+            if facto == 0 || idx == n {
+                break facto % m;
+            }
+            idx += 1;
+        }
+    };
+    writeln!(output, "{}", ans)?;
+    print!("{}", output);
+    Ok(())
+}
