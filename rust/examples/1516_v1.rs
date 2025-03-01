01f4e30708572fdd30161dd730175d0dbe462783
Kim, Hyeonseo <dodok8@gmail.com>
2025-03-01 13:36:13 +0900

    AC 1516

--- rust/examples/1516_v1.rs
+++ rust/examples/1516_v1.rs
@@ -9,42 +9,47 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut output = String::new();
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
-    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<i64>);
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<i32>);
 
     let n = input.next().unwrap() as usize;
 
     let mut build_times = vec![0; n + 1];
     let mut edges: Vec<Vec<usize>> = vec![vec![]; n + 1];
-
-    let mut starts = Vec::new();
+    let mut in_degrees = vec![0; n + 1];
 
     for idx in 1..=n {
         build_times[idx] = input.next().unwrap() as usize;
-        for (jdx, val) in input.by_ref().enumerate() {
+        for val in input.by_ref() {
             if val == -1 {
-                if jdx == 0 {
-                    starts.push(idx);
-                }
                 break;
             } else {
                 let val = val as usize;
                 edges[val].push(idx);
+                in_degrees[idx] += 1;
             }
         }
     }
 
     let mut tot_min_times = vec![0usize; n + 1];
-
     let mut travel_q = VecDeque::new();
-    for start_idx in starts {
-        travel_q.push_back((0, start_idx));
-    }
-    while !travel_q.is_empty() {
-        let (curr_t, curr_v) = travel_q.pop_front().unwrap();
-        tot_min_times[curr_v] = (curr_t + build_times[curr_v]).max(tot_min_times[curr_v]);
 
+    for idx in 1..=n {
+        if in_degrees[idx] == 0 {
+            travel_q.push_back(idx);
+            tot_min_times[idx] = build_times[idx];
+        }
+    }
+
+    while !travel_q.is_empty() {
+        let curr_v = travel_q.pop_front().unwrap();
         for &next_v in &edges[curr_v] {
-            travel_q.push_back((tot_min_times[curr_v], next_v));
+            tot_min_times[next_v] =
+                tot_min_times[next_v].max(tot_min_times[curr_v] + build_times[next_v]);
+
+            in_degrees[next_v] -= 1;
+            if in_degrees[next_v] == 0 {
+                travel_q.push_back(next_v);
+            }
         }
     }
     for tot_min_time in tot_min_times.iter().skip(1) {
