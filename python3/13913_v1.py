2fa6378ec7f11784c0a5c2ebf76924a8b5ad2bf0
Kim, Hyeonseo <dodok8@gmail.com>
2025-03-17 14:49:52 +0900

    WA 5% 13913

--- python3/13913_v1.py
+++ python3/13913_v1.py
@@ -2,33 +2,38 @@ from collections import deque
 
 n, k = map(int, input().split())
 
-dists = [-1 for idx in range(100001)]
-parents = [0 for idx in range(100001)]
-parents[n] = -1
-dists[n] = 0
+if n > k:
+    print(n - k)
+    print(*[int(x) for x in range(n, k - 1, -1)])
 
-tq = deque()
-tq.append((0, n))
+else:
+    dists = [-1 for idx in range(100001)]
+    parents = [0 for idx in range(100001)]
+    parents[n] = -1
+    dists[n] = 0
 
-while tq:
-    dist, x = tq.popleft()
+    tq = deque()
+    tq.append((0, n))
 
-    for nx in [x - 1, x + 1, x * 2]:
-        if nx < 0 or nx >= 100000:
-            continue
-        if dists[nx] == -1:
-            dists[nx] = dist + 1
-            parents[nx] = x
-            tq.append((dists[nx], nx))
+    while tq:
+        dist, x = tq.popleft()
 
-print(dists[k])
+        for nx in [x - 1, x + 1, x * 2]:
+            if nx < 0 or nx >= 100000:
+                continue
+            if dists[nx] == -1:
+                dists[nx] = dist + 1
+                parents[nx] = x
+                tq.append((dists[nx], nx))
 
-x = k
-ans = list()
-ans.append(x)
-while parents[x] != -1:
-    ans.append(parents[x])
-    x = parents[x]
-ans.reverse()
+    print(dists[k])
 
-print(*ans)
+    x = k
+    ans = list()
+    ans.append(x)
+    while parents[x] != -1:
+        ans.append(parents[x])
+        x = parents[x]
+    ans.reverse()
+
+    print(*ans)
