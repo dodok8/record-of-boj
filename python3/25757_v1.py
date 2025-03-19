e78db98b74aa4ae79481c2648d1ff35a01a8e45c
Kim, Hyeonseo <dodok8@gmail.com>
2025-03-19 15:41:36 +0900

    RTE 25757

---
+++ python3/25757_v1.py
@@ -0,0 +1,25 @@
+# 임스와 함께하는 미니게임
+
+from sys import stdin
+
+
+def read():
+    return stdin.readline().rstrip()
+
+
+n, game = read.split()
+
+n = int(n)
+
+people = set()
+
+for _ in range(n):
+    people.add(read())
+
+match game:
+    case "Y":
+        print(len(people))
+    case "F":
+        print(len(people) // 2)
+    case _:
+        print(len(people) // 3)
