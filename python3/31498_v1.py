6d9a4d601023b1465aeb314ccda4cf8013f96cf8
Kim, Hyeonseo <dodok8@gmail.com>
2025-05-17 17:25:28 +0900

    WA 45% 31498

--- python3/31498_v1.py
+++ python3/31498_v1.py
@@ -20,6 +20,7 @@ def get_doll_d(n: int):
 
 
 def get_move_cnt():
+    # 언제 집에 도착하는지 판단 하는 함수
     cnt = -1
 
     start = 0
@@ -28,22 +29,26 @@ def get_move_cnt():
     if k == 0:
         end = math.ceil(a / b)
     else:
-        end = math.floor(b / k) + 1
+        end = math.floor(b / k)
 
     while start <= end:
         mid = (start + end) // 2
 
-        curr_toka = a - get_doll_d(mid)
+        # print(f"start: {start} end: {end} mid: {mid}")
+
+        curr_toka = a - get_toka_d(mid)
+
+        # print(f"curr_toka: {curr_toka}")
 
         if curr_toka <= 0:
+            # 토카가 집에 도착한 경우이니 더 작은 범위에서 탐색
+            cnt = mid
+
             start = start
             end = mid - 1
-
-            cnt = mid
         else:
             start = mid + 1
             end = end
-
     return cnt
 
 
@@ -53,7 +58,6 @@ if t == -1:
     print(-1)
 else:
     curr_doll = c - get_doll_d(t)
-
     if curr_doll > 0:
         print(t)
     else:
