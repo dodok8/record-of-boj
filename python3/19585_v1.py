41b9ec72dba8c91221fd2705fc68b923092ce234
Kim, Hyeonseo <dodok8@gmail.com>
2025-06-13 18:31:40 +0900

    AC 19585

--- python3/19585_v1.py
+++ python3/19585_v1.py
@@ -10,7 +10,7 @@ class Trie:
         self.tree = {}
         self.is_word = is_word
 
-    def insert(self, word: list[int]):
+    def insert(self, word: str):
         curr_node = self
         for char in word:
             if char not in curr_node.tree:
@@ -18,11 +18,8 @@ class Trie:
             curr_node = curr_node.tree[char]
         curr_node.is_word = True
 
-    def find(self, word: list[int]):
+    def find(self, word: str, result: list[int]):
         # 닉네임에서 색 이름이 아니게 되는 인덱스를 result 에 넣기
-
-        result = list()
-
         curr_node = self
         for idx, char in enumerate(word):
             if curr_node.is_word:
@@ -32,47 +29,34 @@ class Trie:
             else:
                 break
 
-        return result
-
 
 num_c, num_n = map(int, read().split())
 
 color_trie = Trie(False)
 
 for idx in range(num_c):
-    color = list(map(ord, read()))
+    color = read()
     color_trie.insert(color)
 
-# 닉네임을 Trie에 저장
-nickname_trie = Trie(False)
+nickname_set = set()
+
 for idx in range(num_n):
-    nickname = list(map(ord, read()))
-    nickname_trie.insert(nickname)
+    nickname = read()
+    nickname_set.add(nickname)
 
 num_q = int(read())
 
 for _ in range(num_q):
-    team = list(map(ord, read()))
+    team = read()
 
-    color_result = color_trie.find(team)
+    color_result = []
+    color_trie.find(team, color_result)
 
     result = False
 
     for cdx in color_result:
-        # 남은 부분이 닉네임인지 확인
         remaining = team[cdx + 1 :]
-
-        # 남은 문자열이 nickname_trie에 있는지 확인
-        curr_node = nickname_trie
-        is_nickname = True
-
-        for char in remaining:
-            if char not in curr_node.tree:
-                is_nickname = False
-                break
-            curr_node = curr_node.tree[char]
-
-        if is_nickname and curr_node.is_word:
+        if remaining in nickname_set:
             result = True
             break
 
