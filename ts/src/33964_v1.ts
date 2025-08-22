369dc1f4708215af0c70225a767f1ae7e300806d
Kim, Hyeonseo <dodok8@gmail.com>
2025-08-22 20:47:44 +0900

    WA 33964

---
+++ ts/src/33964_v1.ts
@@ -0,0 +1,27 @@
+// 레퓨닛의 덧셈
+
+const fs = require('fs')
+
+function* inputIterator<T>(arr: Array<T>) {
+  for (const num of arr) {
+    yield num
+  }
+}
+
+function main() {
+  let input_path = '/dev/stdin'
+  const input = inputIterator(
+    fs.readFileSync(input_path).toString().split(/\s/) as string[]
+  )
+
+  const x = Number(input.next().value)
+  const y = Number(input.next().value)
+
+  
+  const repunitX = parseInt("1".repeat(x))
+  const repunitY = parseInt("1".repeat(x))
+
+  console.log(repunitX + repunitY)
+}
+
+main()
