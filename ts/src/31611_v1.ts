d0286bdc455f7c9369b59aa787934edc75a49041
Hyeonseo Kim <dodok8@gmail.com>
2025-09-16 16:19:45 +0900

    AC 31611

--- ts/src/31611_v1.ts
+++ ts/src/31611_v1.ts
@@ -1,28 +1,27 @@
-"const fs = require('fs')",
+const fs = require("fs");
 
-"function* inputIterator<T>(arr: Array<T>) {",
-"  for (const num of arr) {",
-"    yield num",
-"  }",
-"}",
+function main() {
+  function* inputIterator<T>(arr: Array<T>) {
+    for (const num of arr) {
+      yield num;
+    }
+  }
+  let input_path = "/dev/stdin";
+  try {
+    if (process.env.LOCAL) {
+      input_path = "/workspaces/record-of-boj/input/31611_v1";
+    }
+  } catch {}
+  let input = inputIterator(
+    fs
+      .readFileSync(input_path)
+      .toString()
+      .split(/\s/)
+      .map((x: any) => Number(x)),
+  );
+  let x = input.next().value as number;
 
-"function main() {",
-"  let input_path = '/dev/stdin'",
-"  try {",
-"    if (process.env.LOCAL) {",
-"      input_path = '/workspaces/record-of-boj/input/31611_v1'",
-"    }",
-"  } catch {}",
-"  let input = inputIterator(",
-"    fs",
-"      .readFileSync(input_path)",
-"      .toString()",
-"      .split(/\\s/)",
-"      .map((x: any) => Number(x))",
-"  )",
-"  let x = input.next().value as number",
+  console.log(x % 7 == 2 ? "1" : "0");
+}
 
-"  console.log(x % 7 == 2 ? '1' : '0')",
-"}",
-
-"main()",
+main();
