bbfd5678a7e55a283f44eff1b2114af695e7a9b6
Hyeonseo Kim <dodok8@gmail.com>
2025-10-17 22:16:04 +0900

    RTE 82% 1654

---
+++ python3/1654_v1.py
@@ -0,0 +1,27 @@
+# 랜선 자르기
+
+from sys import stdin
+
+read = lambda: stdin.readline().rstrip()
+
+k, n = map(int, read().split())
+
+wires = [int(read()) for _ in range(k)]
+
+start = 0
+end = max(wires)
+
+max_p = 0
+while start <= end:
+    mid = (start + end) // 2
+    num_wire = 0
+    for wire in wires:
+        num_wire += wire // mid
+    if num_wire >= n:
+        start = mid + 1
+        if mid > max_p:
+            max_p = mid
+    else:
+        end = mid - 1
+
+print(max_p)
