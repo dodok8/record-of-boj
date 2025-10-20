d739ce774bc4c561f72ac0e39d6e4ca9c7a6fd63
Hyeonseo Kim <dodok8@gmail.com>
2025-10-20 20:47:56 +0900

    AC 9465_v1

--- python3/9465_v1.py
+++ python3/9465_v1.py
@@ -16,18 +16,24 @@ for _ in range(t):
         stickers.append(list(map(int, read().split())))
 
     dp = [[0 for _ in range(n)] for __ in range(2)]
-    dp[UP][0] = stickers[UP][0]
-    dp[DOWN][0] = stickers[DOWN][0]
+    if n > 1:
+        dp[UP][0] = stickers[UP][0]
+        dp[DOWN][0] = stickers[DOWN][0]
+        dp[UP][1] = stickers[UP][1] + dp[DOWN][0]
+        dp[DOWN][1] = stickers[DOWN][1] + dp[UP][0]
 
-    for k in range(1, n):
-        dp[UP][k] = max(
-            dp[DOWN][k - 1] + stickers[UP][k],
-            max(dp[UP][k - 2], dp[DOWN][k - 2]) + stickers[UP][k],
-        )
-        dp[DOWN][k] = max(
-            dp[UP][k - 1] + stickers[DOWN][k],
-            max(dp[UP][k - 2], dp[DOWN][k - 2]) + stickers[DOWN][k],
-        )
+        for k in range(2, n):
+            dp[UP][k] = max(
+                dp[DOWN][k - 1] + stickers[UP][k],
+                max(dp[UP][k - 2], dp[DOWN][k - 2]) + stickers[UP][k],
+            )
+            dp[DOWN][k] = max(
+                dp[UP][k - 1] + stickers[DOWN][k],
+                max(dp[UP][k - 2], dp[DOWN][k - 2]) + stickers[DOWN][k],
+            )
 
-    ans = max(dp[DOWN][n - 1], dp[UP][n - 1])
-    print(ans)
+        ans = max(dp[DOWN][n - 1], dp[UP][n - 1])
+        print(ans)
+    else:
+        ans = max(stickers[UP][0], stickers[DOWN][0])
+        print(ans)
